b55bcebcf33beba8f47670ab555bbde68c0a88f5
xiexiie <xyj0107@outlook.com>
2024-12-19 22:20:24 +0800

    update test.py branch1

--- myScripts/test.py
+++ myScripts/test.py
@@ -1,11 +1,5 @@
-# 这就是一个测试文件
+# 这就是一个git测试文件
 
-import numpy
+print("git branch1")
 
-print("Hello world!")
 
-print("Hello world!  branch2")
-
-print("Hello world!  branch3")
-
-print("Hello world!  branch-local-test3")
